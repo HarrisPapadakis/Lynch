f90d52d23fd495ced461c1f8bfa0fd7be4203fbd
Harris_Papadakis <162270644+HarrisPapadakis@users.noreply.github.com>
2024-11-20 18:47:23 +0200

    Add files via upload

---
+++ Show_nums_1_to_100.cs
@@ -0,0 +1,19 @@
+//Show numbers from 1-100
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Show numbers from 1 until 100 ===");
+
+        // Use repetition to display numbers
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        // End of program
+        Console.WriteLine("=== End of program ===");
+    }
+}
